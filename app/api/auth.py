44bbdddef8c8ab2269d232df0bd6a7a6611ace85
Maria Eugenia <blueugenia@gmail.com>
2025-06-09 13:30:20 -0500

    Primer commit: subiendo proyecto GestionFormacion

---
+++ app/api/auth.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends,HTTPException
+from sqlalchemy.orm import Session
+from core.dependencies import authenticate_user
+from app.schemas.auth import AccessTokenResponse
+from core.security import create_access_token
+from core.database import get_db
+from fastapi.security import OAuth2PasswordRequestForm
+
+
+router = APIRouter()
+
+@router.post("/token", response_model=AccessTokenResponse)
+def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Session = Depends(get_db)
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Datos Incorrectos en email o password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = create_access_token(
+        data={"sub": user.id_usuario, "rol":user.id_rol}
+    )
+    return AccessTokenResponse(access_token=access_token)
+
